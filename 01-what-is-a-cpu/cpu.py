cc0527367bf328b16f5b36b9e622f655523e585e
malcata <malcata@users.noreply.github.com>
2026-09-18 10:07:28 +0100

    Added some tips to help the students

--- 01-what-is-a-cpu/cpu.py
+++ 01-what-is-a-cpu/cpu.py
@@ -1,4 +1,5 @@
 # Create a list of all the components, registers and buses of a CPU
 # Print the list, one per line
-# Print a counter for the calculated total list elements
+# Print the calculated total list elements
+# tips: use a for cycle and len
 
